f607f546e4e1eb77260f973c9e8bcae856397208
Bibek <subedibibek200@gmail.com>
2022-11-29 13:02:07 +0545

    first commit

---
+++ components/SuggestedAcc.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SuggestedAcc = () => {
+  return (
+    <div>SuggestedAcc</div>
+  )
+}
+
+export default SuggestedAcc
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,32 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState,useEffect } from 'react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+
+const App = ({ Component, pageProps }: AppProps) =>  {
+  const [server, setServer] = useState(true)
+
+  useEffect(() => {
+    setServer(false)
+  })
+  if(server) return null
+
+  return (
+    <div>
+      <Navbar />
+      <div className=' flex gap-6 md:gap-20'>
+        <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
+           <Sidebar />
+        </div>
+        <div className=' mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
+
+        <Component {...pageProps} />
+        </div>
+      </div>
+    </div>
+  ) 
 }
+
+export default App
