6c57cfbd0ca16542c580a607604e74bdc845d006
Bibek <subedibibek200@gmail.com>
2022-11-30 12:17:42 +0545

    first

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
+import type { NextPage } from "next"
+import axios from 'axios'
 
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <div className="text-3xl font-bold underline">
       VideoApp
     </div>
   )
 }
+
+export const getServerSideProps = async () => {
+  const response = await axios.get(`http://localhost:3000/api/post`)
+  console.log(response.data.name);
+
+  return{
+    props:{}
+  }
+  
+}
+
+export default Home
